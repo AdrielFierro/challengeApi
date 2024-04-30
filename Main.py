1b434e5436c5bf17a868634fc64557042f57322e
AdrielFierro <adri.fierro@outlook.com>
2024-04-30 14:09:17 -0300

    API basica terminada

--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI,Query
+from fastapi import FastAPI,Query,HTTPException
 from pydantic import BaseModel
 from typing import Optional
 import json
 app = FastAPI()
+
+#libro va a ser siempre el constructor que paso para modificar luego el Json
+#libros va a ser el nombre y variable del json con los datos
+#Libro es el Objeto que adentro esta construido con los datos para cargar al Json
 
 class Libro(BaseModel):
     id: Optional[int] = None
@@ -12,7 +16,7 @@ class Libro(BaseModel):
     resumen: str
 
 with open('libros.json','r') as f:
-    libros= json.load(f)['libros']
+    libros= json.load(f)
 
 
 
@@ -50,9 +54,42 @@ def añadir_libro(libro: Libro):
         json.dump(libros,f)
     return new_libro
 
-# @app.delete("/libros")
-# def eliminar_libro():
-#     return print("wip")
+
+@app.put('/changeLibro')
+def cambiar_libro(libro:Libro):
+    new_libro = {
+"id": libro.id,
+"nombre": libro.nombre,
+"autor": libro.autor,
+"categoria": libro.categoria,
+"resumen":libro.resumen
+    }
+    libro_lista = [l for l in libros if l['id'] == libro.id]
+    if len(libro_lista) > 0:
+        libros.remove(libro_lista[0])
+        libros.append(new_libro)
+        with open('libros.json','w') as f:
+            json.dump(libros,f)
+        return new_libro
+    else:
+        return HTTPException(status_code=404, detail="no existe un libro con ese ID")
+
+
+
+@app.delete("/libros")
+def eliminar_libro(l_id:int):
+    delete_libro = [l for l in libros if l['id'] ==l_id]
+    if (len(delete_libro) >0):
+        libros.remove(delete_libro[0])
+        with open('libros.json','w') as f:
+            json.dump(libros,f)
+        raise HTTPException(status_code=204, detail="Libro eliminado")
+    else:
+        raise HTTPException(status_code=404, detail="Id not found")
+        
+
+
+    
 
 
 
